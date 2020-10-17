5f5e0152449d19f42edbd538f7144aaf051c7574
Sarah <sgriggs177@yahoo.co.uk>
2020-10-17 07:38:33 +0100

    login page added

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib import auth, messages
 from django.shortcuts import render, redirect, reverse
+from .forms import UserLoginForm
 
 # Create your views here.
 
@@ -12,3 +14,19 @@ def logout(request):
     """Log the user out"""
     auth.logout(request)
     return redirect(reverse('welcome'))
+
+
+def login(request):
+    """Return a login page"""
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            messages.success(request, "You have successfully logged in!")
+            if user:
+                auth.login(user=user, request=request)
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm(),
+        return render(request, 'login.html', {"login_form": login_form})
