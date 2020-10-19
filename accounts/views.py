33e332df201d889b6116b9edf9eebf7905575fe8
Sarah <sgriggs177@yahoo.co.uk>
2020-10-19 06:06:52 +0100

    login page sorted

--- accounts/views.py
+++ accounts/views.py
@@ -1,61 +1,24 @@
-from django.shortcuts import render, redirect, reverse
-from django.contrib import auth, messages
-from django.contrib.auth.models import User
-from accounts.forms import UserLoginForm, UserRegistrationForm
-
-# Create your views here.
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 
 def welcome(request):
-    """Return the welcome.html file"""
     return render(request, 'welcome.html')
 
 
-def logout(request):
-    """Log the user out"""
-    auth.logout(request)
-    messages.success((request, "You have successfully been logged off"))
-    return redirect(reverse('welcome'))
-
-
-def login(request):
-    """Return a login page"""
-    if request.method == "POST":
-        login_form = UserLoginForm(request.POST)
-
-        if login_form.is_valid():
-            user = auth.authenticate(username=request.POST['username'],
-                                     password=request.POST['password'])
-
-            if user:
-                auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
-                return redirect(reverse('carer_list'))
-            else:
-                login_form.add_error(None, "Your username or password is incorrect")
-    else:
-        login_form = UserLoginForm()
-        return render(request, 'login.html', {"login_form": login_form})
-
-
 def register(request):
-    """Render the registration page"""
-    registration_form = UserRegistrationForm()
-    if request.user.is_authenticated:
-        return redirect(reverse('logout'))
-    if request.method == "POST":
-        registration_form = UserRegistrationForm(request.POST)
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            messages.success(request, 'Your account has been created successfully')
+            auth_user = authenticate(username=new_user.username,
+                                     password=request.POST['password1'])
+            login(request, auth_user)
+            return HttpResponseRedirect(reverse('ticket_list'))
 
-        if registration_form.is_valid():
-            registration_form.save()
-
-            user = auth.authenticate(username=request.POST['username'],
-                                     password=request.POST['password'])
-            if user:
-                auth.login(request, user=user)
-                messages.success(request, "You have successfully registered")
-                return redirect(reverse('index'))
-        else:
-            messages.error(request, "Unable to register your account at this time")
-
-    return render(request, 'register.html', {'registration_form': registration_form})
+    return render(request, 'register.html', {'form': form})
--- adeptcare/settings.py
+++ adeptcare/settings.py
@@ -156,12 +156,14 @@ STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
