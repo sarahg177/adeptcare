9517e0505283551430642a0b5226b03626d8e59f
Sarah <sgriggs177@yahoo.co.uk>
2020-10-23 11:44:35 +0100

    setting changed

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "hsv6m14a8pbx0zrqvmph91d_eg8$0)qoz3fbyti(s6facnormt")
-os.environ.setdefault("DATABASE_URL", "postgres://knicttqmdifgqx:96a71eeaaee41dbbb0ea3d62d0fc9e98c36029c6497c2de5c73fdd5c29ca77c0@ec2-54-170-123-247.eu-west-1.compute.amazonaws.com:5432/d8dod5k7o8m5b8")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAQVVT4V3NWBUQ53GC")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
